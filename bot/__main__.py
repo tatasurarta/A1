52b776dc2977dc471d5eb54e12ab3953df10d06a
tatasurarta <91466068+tatasurarta@users.noreply.github.com>
2021-11-02 00:14:32 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -17,7 +17,7 @@ from telegram.ext import Filters, InlineQueryHandler, MessageHandler, CommandHan
 from telegram.utils.helpers import escape_markdown
 from telegraph import Telegraph
 from wserver import start_server_async
-from bot import bot, app, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, IS_VPS, PORT, alive, web, OWNER_ID, AUTHORIZED_CHATS, telegraph_token,
+from bot import bot, app, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, IS_VPS, PORT, alive, web, OWNER_ID, AUTHORIZED_CHATS, telegraph_token
 from bot.helper.ext_utils import fs_utils
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import *
