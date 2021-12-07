919a0232a1180578b7b0da990c24ae62ec43d41e
tatasurarta <91466068+tatasurarta@users.noreply.github.com>
2021-12-07 14:49:43 +0700

    Update search.py

--- bot/modules/search.py
+++ bot/modules/search.py
@@ -1,26 +1,107 @@
-import math
-import qbittorrentapi as qba
-from telegram import InlineKeyboardMarkup
-from telegram.ext import CallbackQueryHandler, CommandHandler
+import requests
+import itertools
+import time
+import html
 
-from bot import dispatcher, LOGGER, get_client, search_dict_lock, search_dict
-from bot.helper.telegram_helper.message_utils import editMessage, sendMessage
-from bot.helper.ext_utils.bot_utils import new_thread, get_readable_file_size
+from urllib.parse import quote
+from telegram import InlineKeyboardMarkup
+from telegram.ext import CommandHandler, CallbackQueryHandler
+
+from bot import dispatcher, LOGGER, SEARCH_API_LINK, get_client, SEARCH_PLUGINS
+from bot.helper.ext_utils.telegraph_helper import telegraph
+from bot.helper.telegram_helper.message_utils import editMessage, sendMessage, sendMarkup
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.ext_utils.bot_utils import get_readable_file_size
 from bot.helper.telegram_helper import button_build
 
+PLUGINS = []
 
-@new_thread
-def search(update, context):
+SITES = {
+    "1337x": "1337x",
+    "nyaasi": "NyaaSi",
+    "yts": "YTS",
+    "piratebay": "PirateBay",
+    "torlock": "Torlock",
+    "eztv": "EzTvio",
+    "tgx": "TorrentGalaxy",
+    "rarbg": "Rarbg",
+    "ettv": "Ettv",
+    "all": "All"
+}
+
+SEARCH_LIMIT = 300
+
+def torser(update, context):
+    user_id = update.message.from_user.id
     try:
         key = update.message.text.split(" ", maxsplit=1)[1]
+    except IndexError:
+        return sendMessage("Send a search key along with command", context.bot, update)
+    if SEARCH_API_LINK is not None and SEARCH_PLUGINS is not None:
+        buttons = button_build.ButtonMaker()
+        buttons.sbutton('Api', f"torser {user_id} api")
+        buttons.sbutton('Plugins', f"torser {user_id} plugin")
+        buttons.sbutton("Cancel", f"torser {user_id} cancel")
+        button = InlineKeyboardMarkup(buttons.build_menu(2))
+        sendMarkup('Choose tool to search:', context.bot, update, button)
+    elif SEARCH_API_LINK is not None and SEARCH_PLUGINS is None:
+        button = api_buttons(user_id)
+        sendMarkup('Choose site to search:', context.bot, update, button)
+    elif SEARCH_API_LINK is None and SEARCH_PLUGINS is not None:
+        button = plugin_buttons(user_id)
+        sendMarkup('Choose site to search:', context.bot, update, button)
+    else:
+        return sendMessage("Tidak ada tautan API atau PLUGIN pencarian yang ditambahkan untuk fungsi ini", context.bot, update)
+
+def torserbut(update, context):
+    query = update.callback_query
+    user_id = query.from_user.id
+    message = query.message
+    key = message.reply_to_message.text.split(" ", maxsplit=1)[1]
+    data = query.data
+    data = data.split(" ")
+    if user_id != int(data[1]):
+        query.answer(text="Not Yours!", show_alert=True)
+    elif data[2] == 'api':
+        button = api_buttons(user_id)
+        editMessage('Choose site to search:', message, button)
+    elif data[2] == 'plugin':
+        button = plugin_buttons(user_id)
+        editMessage('Choose site to search:', message, button)
+    elif data[2] != "cancel":
+        query.answer()
+        site = data[2]
+        tool = data[3]
+        if tool == 'api':
+            editMessage(f"<b>Mencari untuk <i>{key}</i>\nSitus Torrent:- <i>{SITES.get(site)}</i></b>", message)
+        else:
+            editMessage(f"<b>Mencari untuk <i>{key}</i>\nSitus Torrent:- <i>{site.capitalize()}</i></b>", message)
+        search(key, site, message, tool)
+    else:
+        query.answer()
+        editMessage("Pencarian telah dibatalkan!", message)
+
+def search(key, site, message, tool):
+    LOGGER.info(f"Searching: {key} from {site}")
+    if tool == 'api':
+        api = f"{SEARCH_API_LINK}/api/{site}/{key}"
+        try:
+            resp = requests.get(api)
+            search_results = resp.json()
+            if site == "all":
+                search_results = list(itertools.chain.from_iterable(search_results))
+            if isinstance(search_results, list):
+                msg = f"<b>Ditemukan {min(len(search_results), SEARCH_LIMIT)}</b>"
+                msg += f" <b>hasil untuk <i>{key}</i>\nSitus Torrent:- <i>{SITES.get(site)}</i></b>"
+            else:
+                return editMessage(f"Tidak ada hasil untuk <i>{key}</i>\nSitus Torrent:- <i>{SITES.get(site)}</i>", message)
+        except Exception as e:
+            editMessage(str(e), message)
+    else:
         client = get_client()
-        search = client.search_start(pattern=str(key), plugins='all', category='all')
-        srchmsg = sendMessage("Mencari...", context.bot, update)
-        user_id = update.message.from_user.id
+        search = client.search_start(pattern=str(key), plugins=str(site), category='all')
         search_id = search.id
-        LOGGER.info(f"Pencarian qBittorrent: {key}")
         while True:
             result_status = client.search_status(search_id=search_id)
             status = result_status[0].status
@@ -30,94 +111,120 @@ def search(update, context):
         search_results = dict_search_results.results
         total_results = dict_search_results.total
         if total_results != 0:
-            total_pages = math.ceil(total_results/3)
-            msg = getResult(search_results)
-            buttons = button_build.ButtonMaker()
-            if total_results > 3:
-                msg += f"<b>Halaman: </b>1/{total_pages} | <b>Hasil: </b>{total_results}"
-                buttons.sbutton("Kembali", f"srchprev {user_id} {search_id}")
-                buttons.sbutton("Lanjut", f"srchnext {user_id} {search_id}")
-            buttons.sbutton("Tutup", f"closesrch {user_id} {search_id}")
-            button = InlineKeyboardMarkup(buttons.build_menu(2))
-            editMessage(msg, srchmsg, button)
-            with search_dict_lock:
-                search_dict[search_id] = client, search_results, total_results, total_pages, 1, 0
+            msg = f"<b>Ditemukan {min(total_results, SEARCH_LIMIT)}</b>"
+            msg += f" <b>hasil untuk <i>{key}</i>\nSitus Torrent:- <i>{site.capitalize()}</i></b>"
         else:
-            editMessage(f"Tidak ada hasil untuk <i>{key}</i>", srchmsg)
-    except IndexError:
-        sendMessage("Send a search key along with command", context.bot, update)
-    except Exception as e:
-        LOGGER.error(str(e))
-
-def searchPages(update, context):
-    query = update.callback_query
-    user_id = query.from_user.id
-    data = query.data
-    data = data.split(" ")
-    search_id = int(data[2])
-    if user_id != int(data[1]):
-        query.answer(text="Bukan Buat Kamu MASS!", show_alert=True)
-        return
-    with search_dict_lock:
-        try:
-            client, search_results, total_results, total_pages, pageNo, start = search_dict[search_id]
-        except:
-            query.answer(text="Old Result", show_alert=True)
-            query.message.delete()
-            return
-    if data[0] == "srchnext":
-        if pageNo == total_pages:
-            start = 0
-            pageNo = 1
-        else:
-            start += 3
-            pageNo += 1
-    elif data[0] == "srchprev":
-        if pageNo == 1:
-            pageNo = total_pages
-            start = 3 * (total_pages -1)
-        else:
-            pageNo -= 1
-            start -= 3
-    elif data[0] == "closesrch":
-        client.search_delete(search_id)
-        client.auth_log_out()
-        with search_dict_lock:
-            try:
-                del search_dict[search_id]
-            except:
-                pass
-        query.message.delete()
-        return
-    with search_dict_lock:
-        search_dict[search_id] = client, search_results, total_results, total_pages, pageNo, start
-    msg = getResult(search_results, start=start)
-    msg += f"<b>Halaman: </b>{pageNo}/{total_pages} | <b>Hasil: </b>{total_results}"
+            return editMessage(f"Tidak ada hasil untuk <i>{key}</i>\nSitus Torrent:- <i>{site.capitalize()}</i>", message)
+    link = getResult(search_results, key, message, tool)
     buttons = button_build.ButtonMaker()
-    buttons.sbutton("Kembali", f"srchprev {user_id} {search_id}")
-    buttons.sbutton("Lanjut", f"srchnext {user_id} {search_id}")
-    buttons.sbutton("Tutup", f"closesrch {user_id} {search_id}")
-    button = InlineKeyboardMarkup(buttons.build_menu(2))
-    query.answer()
-    editMessage(msg, query.message, button)
+    buttons.buildbutton("🔎 Cek Hasilnya BOS!", link)
+    button = InlineKeyboardMarkup(buttons.build_menu(1))
+    editMessage(msg, message, button)
+    if tool != 'api':
+        client.search_delete(search_id=search_id)
 
-def getResult(search_results, start=0):
-    msg = ""
-    for index, result in enumerate(search_results[start:], start=1):
-        msg += f"<a href='{result.descrLink}'>{result.fileName}</a>\n"
-        msg += f"<b>Ukuran: </b>{get_readable_file_size(result.fileSize)}\n"
-        msg += f"<b>Seeders: </b>{result.nbSeeders} | <b>Leechers: </b>{result.nbLeechers}\n"
-        msg += f"<b>Link: </b><code>{result.fileUrl}</code>\n\n"
-        if index == 3:
+def getResult(search_results, key, message, tool):
+    telegraph_content = []
+    msg = f"<h4>Search Result For {key}</h4>"
+    for index, result in enumerate(search_results, start=1):
+        if tool == 'api':
+            try:
+                msg += f"<code><a href='{result['Url']}'>{html.escape(result['Name'])}</a></code><br>"
+                if "Files" in result.keys():
+                    for subres in result['Files']:
+                        msg += f"<b>Kualitas: </b>{subres['Quality']} | <b>Ukuran: </b>{subres['Size']}<br>"
+                        try:
+                            msg += f"<b>Bagikan tautan ke</b> <a href='http://t.me/share/url?url={subres['Torrent']}'>Telegram</a><br>"
+                            msg += f"<b>Tautan: </b><code>{subres['Torrent']}</code><br>"
+                        except KeyError:
+                            msg += f"<b>Bagikan magnet ke</b> <a href='http://t.me/share/url?url={subres['Magnet']}'>Telegram</a><br>"
+                else:
+                    msg += f"<b>Ukuran: </b>{result['Size']}<br>"
+                    msg += f"<b>Seeders: </b>{result['Seeders']} | <b>Leechers: </b>{result['Leechers']}<br>"
+            except KeyError:
+                pass
+            try:
+                msg += f"<b>Bagikan magnet ke</b> <a href='http://t.me/share/url?url={quote(result['Magnet'])}'>Telegram</a><br><br>"
+            except KeyError:
+                msg += "<br>"
+        else:
+            msg += f"<a href='{result.descrLink}'>{html.escape(result.fileName)}</a><br>"
+            msg += f"<b>Size: </b>{get_readable_file_size(result.fileSize)}<br>"
+            msg += f"<b>Seeders: </b>{result.nbSeeders} | <b>Leechers: </b>{result.nbLeechers}<br>"
+            link = result.fileUrl
+            if link.startswith('magnet:'):
+                msg += f"<b>Bagikan magnet ke</b> <a href='http://t.me/share/url?url={quote(link)}'>Telegram</a><br><br>"
+            else:
+                msg += f"<b>Bagikan url ke</b> <a href='http://t.me/share/url?url={link}'>Telegram</a><br><br>"
+
+        if len(msg.encode('utf-8')) > 39000:
+           telegraph_content.append(msg)
+           msg = ""
+
+        if index == SEARCH_LIMIT:
             break
-    return msg
+
+    if msg != "":
+        telegraph_content.append(msg)
+
+    editMessage(f"<b>Membuat</b> {len(telegraph_content)} <b>Telegraph pages.</b>", message)
+    path = [telegraph.create_page(
+                title='Rumah Awan Torrent Search',
+                content=content
+            )["path"] for content in telegraph_content]
+    time.sleep(0.5)
+    if len(path) > 1:
+        editMessage(f"<b>Mengedit</b> {len(telegraph_content)} <b>Telegraph pages.</b>", message)
+        edit_telegraph(path, telegraph_content)
+    return f"https://telegra.ph/{path[0]}"
+
+def edit_telegraph(path, telegraph_content):
+    nxt_page = 1
+    prev_page = 0
+    num_of_path = len(path)
+    for content in telegraph_content :
+        if nxt_page == 1 :
+            content += f'<b><a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
+            nxt_page += 1
+        else :
+            if prev_page <= num_of_path:
+                content += f'<b><a href="https://telegra.ph/{path[prev_page]}">Prev</a></b>'
+                prev_page += 1
+            if nxt_page < num_of_path:
+                content += f'<b> | <a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
+                nxt_page += 1
+        telegraph.edit_page(
+            path = path[prev_page],
+            title = 'Rumah Awan Torrent Search',
+            content=content
+        )
+    return
+
+def api_buttons(user_id):
+    buttons = button_build.ButtonMaker()
+    for data, name in SITES.items():
+        buttons.sbutton(name, f"torser {user_id} {data} api")
+    buttons.sbutton("Cancel", f"torser {user_id} cancel")
+    button = InlineKeyboardMarkup(buttons.build_menu(2))
+    return button
+
+def plugin_buttons(user_id):
+    buttons = button_build.ButtonMaker()
+    if not PLUGINS:
+        client = get_client()
+        sites = client.search_plugins()
+        for name in sites:
+            PLUGINS.append(name['name'])
+    for siteName in PLUGINS:
+        buttons.sbutton(siteName.capitalize(), f"torser {user_id} {siteName} plugin")
+    buttons.sbutton('All', f"torser {user_id} all plugin")
+    buttons.sbutton("Cancel", f"torser {user_id} cancel")
+    button = InlineKeyboardMarkup(buttons.build_menu(2))
+    return button
 
 
-search_handler = CommandHandler(BotCommands.SearchCommand, search, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-srchnext_handler = CallbackQueryHandler(searchPages, pattern="srchnext", run_async=True)
-srchprevious_handler = CallbackQueryHandler(searchPages, pattern="srchprev", run_async=True)
-delsrch_handler = CallbackQueryHandler(searchPages, pattern="closesrch", run_async=True)
-dispatcher.add_handler(search_handler)
-dispatcher.add_handler(srchnext_handler)
-dispatcher.add_handler(srchprevious_handler)
-dispatcher.add_handler(delsrch_handler)
+torser_handler = CommandHandler(BotCommands.SearchCommand, torser, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+torserbut_handler = CallbackQueryHandler(torserbut, pattern="torser", run_async=True)
+
+dispatcher.add_handler(torser_handler)
+dispatcher.add_handler(torserbut_handler)
